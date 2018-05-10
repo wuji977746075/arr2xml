077d182399afed79d39d074137068662d415d8ed
rainbow <hzboye010@163.com>
2018-05-10 15:45:55 +0800

    test

---
+++ src/util/Xml2arr.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Author      : rainbow <977746075@qq.com>
+ * DateTime    : 2018-05-10 15:26:40
+ * Description : [xml array converter]
+ */
+
+namespace rainbow\util;
+// use
+
+class Xml2arr {
+
+  function test(){
+    echo 'test';
+  }
+}
---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+use rainbow\util\Xml2arr;
+
+(new Xml2arr)->test();
